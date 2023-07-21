708c65c4ffaaf30f4d28b3e945a60cad33887774
spofford999 <james.spofford@hotmail.co.uk>
2023-07-21 19:34:39 +0100

    Updated script to prevent negative numbers

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const banana_scale = (num) => Math.round(num/0.18);
+const banana_scale = (num) => {
+    if(num >0) {
+        return Math.round(num/0.18);
+    }
+
+    else {
+        return "Please enter a positive number!"
+    }
+}
+
 module.exports = { banana_scale };
